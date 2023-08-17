df18d3ce45bf57794993c06e8a11d6d08f6a9582
郎小姐 <langxue_dalang@163.com>
2023-08-17 16:52:07 +0800

    暂时注释子应用注册，手动指定不劫持某个script引入的js

--- src/qiankun/start.js
+++ src/qiankun/start.js
@@ -4,6 +4,16 @@ export default function () {
   start({
     sandbox: {
       experimentalStyleIsolation: true
-    }
+    },
+    async fetch(url, ...args) {
+      if (url === "http://local.leandc.cn:8083/echarts.min.js") {
+        return {
+          async text() {
+            return ""
+          }
+        }
+      }
+      return window.fetch(url, ...args);
+    },
   }); //启动qiankun
 }
