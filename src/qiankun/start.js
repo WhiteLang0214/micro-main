3733649ca2684574e5b58433b9871ff6645bcb69
郎小姐 <langxue_dalang@163.com>
2023-08-23 16:55:17 +0800

    添加子应用bi

--- src/qiankun/start.js
+++ src/qiankun/start.js
@@ -6,15 +6,15 @@ export default function () {
       // strictStyleIsolation: true, // 开启严格的样式隔离模式 这样子应用会找不到dom
       experimentalStyleIsolation: true
     },
-    async fetch(url, ...args) {
-      if (url === "http://local.leandc.cn:8083/echarts.min.js") {
-        return {
-          async text() {
-            return ""
-          }
-        }
-      }
-      return window.fetch(url, ...args);
-    },
+    // async fetch(url, ...args) {
+    //   if (url === "http://local.leandc.cn:8083/echarts.min.js") {
+    //     return {
+    //       async text() {
+    //         return ""
+    //       }
+    //     }
+    //   }
+    //   return window.fetch(url, ...args);
+    // },
   }); //启动qiankun
 }
