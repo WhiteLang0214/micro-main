b8dc219cfb50da814e3284c5e54674a5bbff8f75
郎小姐 <langxue_dalang@163.com>
2023-09-01 16:54:40 +0800

    顶部tab导航组件

--- src/components/menu/tabMenu.vue
+++ src/components/menu/tabMenu.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="micro-tab-menu">
     <el-button
-      text
-      :type="route.fullPath === i.currentActivePath ? 'primary' : 'info'"
+      :class="route.fullPath === i.currentActivePath ? 'micro-tab-menu-active' : ''"
+      :text="route.fullPath === i.currentActivePath ? true : false"
+      :type="route.fullPath === i.currentActivePath ? 'primary' : 'default'"
+      bg
       v-for="i in routeMatched"
       :key="i.fullPath"
       @click="changeRoute(i)"
@@ -25,6 +27,7 @@ let routeMatched = ref(route.matched);
 
 const changeRoute = (i) => {
   if (i.path !== route.fullPath) {
+    // currentActivePath 是带着不同微应用的baseUrl的
     router.push(i.currentActivePath)
     store.commit("SAVE_CURRENTACTIVEMENU", JSON.stringify({ ...i, currentActivePath: i.currentActivePath }))
   }
@@ -40,5 +43,31 @@ watch(
   }
 )
 </script>
-<style scoped>
+<style scoped lang="scss">
+.micro-tab-menu {
+  padding: 0 10px 10px;
+  .el-button {
+    border-radius: 0;
+    &.is-has-bg,
+    &:active.is-has-bg,
+    &:hover.is-has-bg,
+    &:focus.is-has-bg {
+      background-color: #fff;
+      border: none;
+    }
+    &.micro-tab-menu-active {
+      position: relative;
+      &:after {
+        content: "";
+        display: block;
+        width: 100%;
+        height: 10px;
+        background-color: #fff;
+        position: absolute;
+        left: 0;
+        bottom: -10px;
+      }
+    }
+  }
+}
 </style>
