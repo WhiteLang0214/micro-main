0006eec3e58a8135364ad41e42a9aafbec134d3c
郎小姐 <langxue_dalang@163.com>
2023-09-13 15:12:25 +0800

    production环境

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from "element-plus";
 import 'element-plus/dist/index.css'
-// import "@/assets/element.scss"; // 重设主题色
-import "@/assets/element.css"; // 重设主题色
+import "@/assets/element.scss"; // 重设主题色
+// import "@/assets/element.css"; // 重设主题色
 // import { useCssVar } from "@vueuse/core"
 // console.log("useCssVar", useCssVar)
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
